22b4098135d3209acca593a089288e9038effe01
UnLegitCode <ZubMax2005@yandex.ru>
2023-06-16 22:13:06 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("java")
+}
+
+group = "ru.unlegit"
+version = "1.0"
---
+++ src/main/java/ru/unlegit/githubtest/PidorasUtil.java
@@ -0,0 +1,8 @@
+package ru.unlegit.githubtest;
+
+public final class PidorasUtil {
+
+    public static boolean isPidoras(String coder) {
+        return !coder.equals("UnLegit");
+    }
+}
